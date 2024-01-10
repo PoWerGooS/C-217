32938d1f577d01461997de79aa4866e72f8f8808
PoWerGooS <77084057+PoWerGooS@users.noreply.github.com>
2024-01-09 19:22:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get("/:room", (req, res) => {
 });
 
 app.post("/send-mail", (req, res) => {
-    const to = "pranavmaddipatla21@gmail.com"
-    const url = "www.google.com";
+    const to = req.body.to;
+    const url = req.body.url;
     const mailData = {
         from: "pranavmaddipatla21@gmail.com",
         to: to,
